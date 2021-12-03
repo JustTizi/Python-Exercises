b678b4f318bc09f28b8da5f5626f88ea4e78804b
JustTizi <r0889630@student.thomasmore.be>
2021-12-03 12:54:30 +0100

    Added chapter 8 and 9

--- Chapter 7 - Text Files/Exercise_4.py
+++ Chapter 7 - Text Files/Exercise_4.py
@@ -1,5 +1,5 @@
 def main():
-    with open("D:\\OneDrive\\Documents\\Python-Exercises\\Chapter 7 - Text Files\\FilesChapter7\\irish_song.txt", "r") as file:
+    with open("D:\\School\\Python Essentials 1\\Python-Exercises\\Chapter 7 - Text Files\\FilesChapter7\\irish_song.txt", "r") as file:
         line = file.readline()
         
 
